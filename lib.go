610b7872ac3cb9c41653094eb99fbde6b8482a29
void <git.email.address@googlemail.com>
2025-09-29 01:21:16 +0200

    Added some HTTP and FS stuff

--- lib.go
+++ lib.go
@@ -1,9 +1,21 @@
 package commons
 
 import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 	"runtime"
 	"slices"
 	"sync"
+	"time"
+)
+
+const (
+	httpTimeoutSeconds = 10
+	userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:142.0) Gecko/20100101 Firefox/142.0"
 )
 
 type taskTuple[T any] struct {
@@ -75,4 +87,50 @@ func ParallelMap[A, B any](elements []A, callback func(A) B) []B {
 	}
 	wg.Wait()
 	return output
+}
+
+func CreateDirectory(path string) {
+	err := os.Mkdir(path, 0755)
+	if err != nil && !errors.Is(err, os.ErrExist) {
+		log.Fatalf("Failed to create directory: %v", err)
+	}
+}
+
+func FileExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func Download(url string) (string, error) {
+	client := &http.Client{
+		Transport: &http.Transport{},
+		Timeout: httpTimeoutSeconds * time.Second,
+	}
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		err := fmt.Errorf("Failed to create HTTP request (%s): %v", url, err)
+		return "", err
+	}
+	request.Header.Set("User-Agent", userAgent)
+	response, err := client.Do(request)
+	if err != nil {
+		err := fmt.Errorf("Failed to GET data (%s): %v", url, err)
+		return "", err
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		err := fmt.Errorf("Failed to read response (%s): %v", url, err)
+		return "", err
+	}
+	return string(body), nil
+}
+
+func DownloadFile(url string, path string) error {
+	data, err := Download(url)
+	if err != nil {
+		return err
+	}
+	WriteFile(path, data)
+	return nil
 }
--- string.go
+++ string.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"log"
 	"math"
+	"strings"
 	"strconv"
 )
 
-func IntToString(integer int64) string {
+func IntToString(integer int) string {
+	return strconv.FormatInt(int64(integer), 10)
+}
+
+func Int64ToString(integer int64) string {
 	return strconv.FormatInt(integer, 10)
 }
 
@@ -15,12 +20,28 @@ func FloatToString(number float64) string {
 	return strconv.FormatFloat(number, 'f', 2, 64)
 }
 
-func ParseInt(input string) (int64, error) {
+func ParseInt(input string) (int, error) {
+	output, err := strconv.ParseInt(input, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return int(output), nil
+}
+
+func ParseInt64(input string) (int64, error) {
 	return strconv.ParseInt(input, 10, 64)
 }
 
-func MustParseInt(input string) int64 {
+func MustParseInt(input string) int {
 	output, err := ParseInt(input)
+	if err != nil {
+		log.Fatalf("Failed to parse integer: %s", input)
+	}
+	return output
+}
+
+func MustParseInt64(input string) int64 {
+	output, err := ParseInt64(input)
 	if err != nil {
 		log.Fatalf("Failed to parse integer: %s", input)
 	}
@@ -52,4 +73,8 @@ func FormatMoney(amount float64) string {
 		output = fmt.Sprintf("-%s", output)
 	}
 	return output
+}
+
+func Trim(input string) string {
+	return strings.Trim(input, " \r\t\n")
 }
